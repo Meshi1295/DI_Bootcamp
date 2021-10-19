716a805855f43584581d9b6c0dc7a010b415931a
meshi1295 <meshi1295@gmail.com>
2021-10-19 19:08:11 +0300

    first commit

--- Week3/day3/Daily-Challenge/script.js
+++ Week3/day3/Daily-Challenge/script.js
@@ -21,3 +21,21 @@ input.addEventListener('input', function (){
 );
 
 // .match(/[a-zA-Z]/g)
+
+// stackoverflow -------
+// function isNumberKey(evt){  
+// 	var charCode = (evt.which) ? evt.which : evt.keyCode
+//     if (charCode != 46 && charCode > 31 
+// 	&& (charCode < 48 || charCode > 57))
+//         return false;
+//         return true;
+// 	}
+		   
+//     function ValidateAlpha(evt)
+//     {
+//         var keyCode = (evt.which) ? evt.which : evt.keyCode
+//         if ((keyCode < 65 || keyCode > 90) && (keyCode < 97 || keyCode > 123) && keyCode != 32)
+         
+//         return false;
+//             return true;
+//     }
