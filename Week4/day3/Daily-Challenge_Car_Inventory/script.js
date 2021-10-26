259378d2d3d1207d29b96054a01b0622bdac6da3
meshi1295 <meshi1295@gmail.com>
2021-10-26 18:48:05 +0300

    ex_XP

---
+++ Week4/day3/Daily-Challenge_Car_Inventory/script.js
@@ -0,0 +1,34 @@
+// Daily Challenge: Car Inventory
+
+// Part I
+
+//1.  Create a function getCarHonda(carInventory) that takes a single parameter. carInventory‘s value is an array which is an inventory of cars (see the array of objects below)
+// 2. The function should
+// loop through the array of object and return the first car with the name “Honda”.
+// 3. then, return a string in the format This is a {car_make} {car_model} from {car_year}.
+// Hint : Find an array method that returns the value of the first element in an array that pass a test.
+
+let inventory = [
+    { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+    { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+    { id: 3, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+    { id: 4, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+    { id: 5, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  ];
+
+  function getCarHonda (carInventory){
+      for (let car of inventory){
+          if( car.car_make === carInventory ){
+            return car;
+
+          }
+      }
+  }
+  console.log(getCarHonda('Honda'));
+
+let found = inventory.find(e => {
+    if(e.car_make === 'Honda'){
+        console.log(`This is a ${e.car_make} ${e.car_model} from ${e.car_year}`);
+
+    } 
+})
--- Week4/day3/ExerciseXP/ex_4.js
+++ Week4/day3/ExerciseXP/ex_4.js
@@ -9,7 +9,14 @@ let students = [{name: "Ray", course: "Computer Science", isPassed: true},
                {name: "Jamie", course: "Big Data", isPassed: false}];
 
 
-               let studentsThatPassed = students.filter(e => {
-                   e.isPassed === true
-                   console.log(`Hi ${e.name} Congratulations on completing the course`);
-               })
+            //    let studentsThatPassed = students.filter(e => {
+            //        e.isPassed === true
+            //        console.log(`Hi ${e.name} Congratulations on completing the course`);
+            //    })
+
+
+// Bonus : Chain the filter method with a map method, to congratulate the students with their name and course name (ie. “Good job Jenner, you passed the course in Information Technology”, “Good Job Marco you passed the course in Robotics” ect…)
+
+let studentsThatPassed = students.filter(e => e.isPassed).map(e => `Good job ${e.name}, you passed the course in ${e.course}`)
+
+console.log(studentsThatPassed);
