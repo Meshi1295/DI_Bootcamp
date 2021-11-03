8c1970470b6fd0d7d05c1c6ba3bc93e218f1c562
meshi1295 <meshi1295@gmail.com>
2021-11-03 19:38:23 +0200

    promises

---
+++ Week5/day4/Daily-Challenge_Promises/script.js
@@ -0,0 +1,51 @@
+// Implement a simple version of Promise.all.
+// This function should accept an array of promises and return an array of resolved values.
+// If any of the promises are rejected, the function should catch them.
+
+let userName = prompt('What is yor first name?')
+
+let firstPromise =  new Promise((resolve, reject)=>{
+    if(userName[0] === userName[0].toUpperCase()){
+        return resolve(userName)
+    } else {
+        return reject('your first letter need to be uppercase')
+    }
+})
+
+     
+
+
+
+let user = prompt('What is your last name?')
+
+let secondPromise = new Promise((resolve, reject)=>{
+    if(user[0] === user[0].toUpperCase()){
+        return resolve(user)
+    } else {
+        return reject('your first letter need to be uppercase')
+    }
+})
+     
+
+
+let question = prompt("you love your name? Answer yes or no, small letters pleas")
+
+let promise3 =  new Promise((resolve, reject) => {
+        
+    if(question === "yes"){
+        return  resolve(question)
+    } else if(question === "no") {
+        return reject("Your problem !!!!!!")
+    }
+});
+   
+
+
+
+
+
+
+
+Promise.all([firstPromise,secondPromise,promise3]).then((res) => {
+   console.log(`Your full name is: ${res[0]} ${res[1]} and ${res[2]} i love it`);
+})
