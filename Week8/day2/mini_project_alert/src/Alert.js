101aa341eb4b4adb9c243951efb11fa6d815fe16
meshi1295 <meshi1295@gmail.com>
2021-11-23 17:37:57 +0200

    ex

--- Week8/day2/mini_project_alert/src/Alert.js
+++ Week8/day2/mini_project_alert/src/Alert.js
@@ -1,22 +1,60 @@
 import React from 'react';
 
-class Alert extends React.Component {
-    constructor(){
-        super();
-        this.state ={
+//Exercise 1: Alert
+//Exercise 2: Conditional Rendering
+// const Alert = (props) =>{
+//     const {text,show} = props
 
-        }
-    }
+//     if(show){
+//         return(
+       
+//             <div className="alert alert-danger" role="alert">
+//                  {text}
+//             </div>
+//         )
+//     } else {
+//         return(
+//             <div>
+//             </div>
+//         )
+//     }    
+// }
 
-    render(){
-        return(
-            <div class="alert alert-danger" role="alert">
-                  OMG! Something really bad has happended!
-            </div>
-        )
-    }
 
-    
-}
+//Exercise 3: Smart Alert
+// const Alert = (props) =>{
+//     const {red,orange} = props
+     
+//         return(
+//             <>
+//             <div className={red} role="alert">
+//                 This is a primary alert—check it out!
+//             </div>
+//              <div className={orange} role="alert">
+//              This is a primary alert—check it out!
+//          </div>
+//          </>
+//         )
+//     }    
 
+    //Exercise 4: More Alert Colors
+    const Alert = (props) =>{
+        const {color} = props
+            return(
+                <>
+                <div className={color.red} role="alert">
+                    This is a primary alert—check it out!
+                </div>
+                 <div className={color.orange} role="alert">
+                 This is a primary alert—check it out!
+             </div>
+             <div className={color.green} role="alert">
+                 This is a primary alert—check it out!
+             </div>
+             <div className={color.noColor} role="alert">
+                   This color is undefined
+             </div>
+           </>
+            )              
+        }   
 export default Alert
--- Week8/day2/mini_project_alert/src/App.js
+++ Week8/day2/mini_project_alert/src/App.js
@@ -2,14 +2,19 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Alert from './Alert'
 
-
 function App() {
+  const colorClasses ={
+    red:"alert alert-danger",
+    orange:"alert alert-warning",
+    green:"alert alert-success",
+    noColor:null
+} 
   return (
     <div className="App">
-      <Alert text="OMG! Something really bad has happended!"/>
-     
+      {/* <Alert text="OMG! Something really bad has happened!" show={false}/> */}
+      {/* <Alert text="Well done, you success to sign up!" show={true}/>      */}
+      <Alert color={colorClasses}/>
     </div>
   );
 }
-
 export default App;
