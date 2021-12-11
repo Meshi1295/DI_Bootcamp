f6274a32d5a2184ca3485c960b93d7fd92ced538
meshi1295 <meshi1295@gmail.com>
2021-12-11 18:38:00 +0200

    ex

--- Week9/day5/daily_challenge_movie_app/src/App.js
+++ Week9/day5/daily_challenge_movie_app/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { HashRouter as Router, Route } from 'react-router-dom';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+
+import Landing from './components/home/Landing';
+import Movie from './components/home/Movie';
+
+import store from './store';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/movie/:id" component={Movie} />
+            <Footer />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
---
+++ Week9/day5/daily_challenge_movie_app/src/components/layout/Footer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="footer p-3 mt-4 text-center bg-dark text-light">
+
+            Using <i className="fab fa-react" /> React JS &amp; Redux JS
+            integrated with external movies data API
+            <a
+              href="http://www.omdbapi.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              OMDB
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
